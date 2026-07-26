265fed1e90f2ef36a61f9769daaae640ca8e0b89
IntGrah <git@sl.intgrah.com>
2026-07-26 17:11:34 +0100

    Golf

--- tests/proj-non-structure.lean
+++ tests/proj-non-structure.lean
@@ -5,12 +5,12 @@ inductive Bad : Prop
   | mk1 : False → Bad
   | mk2 : True → Bad
 
-run_cmd liftTermElabM do
-  let decl : Declaration := .thmDecl {
-    name        := `bad
-    levelParams := []
-    type        := mkConst ``False
-    value       := .proj `Bad 0 (mkApp (mkConst ``Bad.mk2) (mkConst ``True.intro))
-  }
+run_meta
   withOptions (debug.skipKernelTC.set · true) do
-    addDecl decl
+    addDecl <|
+      .thmDecl {
+        name := `bad
+        levelParams := []
+        type := mkConst ``False
+        value := .proj `Bad 0 (mkApp (mkConst ``Bad.mk2) (mkConst ``True.intro))
+      }
