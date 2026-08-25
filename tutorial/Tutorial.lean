8d6911739447ccc6ca85c3102b351c232b424905
Rob Simmons <rob@lean-fro.org>
2026-08-25 13:56:38 -0400

    reword more

--- tutorial/Tutorial.lean
+++ tutorial/Tutorial.lean
@@ -1526,7 +1526,9 @@ bad_consts #[`DupInd2]
 Unsafe and partial declarations can't be used in theorems.
 
 Kernels are permitted to automatically reject or decline whenever they see an unsafe or partial declaration (nanoda does this).
-That's reasonable because, if partial or unsafe declarations are unused, `lean4export` will just strip them out.
+That's reasonable if you're using `lean4export` in the common way where you specify specific constants,
+and only the transitive dependencies of those constants are output.
+In that mode, if unsafe or partial declarations aren't used, they simply won't be output at all.
 Other kernels simply ignore unsafe and partial definitions, so any later use of them becomes an undefined constant.
 -/
 
